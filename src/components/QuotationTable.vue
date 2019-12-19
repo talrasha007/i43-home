a47bff2431e0e8a54aa2854bcd0a6e68098c86f9
Tal <talrasha007@gmail.com>
2019-12-20 00:39:26 +0800

    table.

--- src/components/QuotationTable.vue
+++ src/components/QuotationTable.vue
@@ -1,11 +1,12 @@
 <template>
   <v-simple-table v-slot:default>
-    <thead><tr><th>INS</th><th>Ask</th><th>Bid</th></tr></thead>
+    <thead><tr><th>INS</th><th>Ask</th><th>Bid</th><th></th></tr></thead>
     <tbody>
       <tr v-for="t of quotations" :key="t.instrument_id">
         <td>{{t.name}}</td>
-        <td>{{t.ask.toFixed(digits)}}</td>
-        <td>{{t.bid.toFixed(digits)}}</td>
+        <td :class="t.askClass">{{t.ask.toFixed(digits)}}</td>
+        <td :class="t.bidClass">{{t.bid.toFixed(digits)}}</td>
+        <td :class="t.deltaClass">{{t.delta && t.delta.toFixed(digits)}}</td>
       </tr>
     </tbody>
   </v-simple-table>
@@ -28,7 +29,21 @@ export default {
             ask: 1 * q.asks[0][0],
             bid: 1 * q.bids[0][0]
           }))
-          .sort((q1, q2) => q1.name - q2.name);
+          .sort((q1, q2) => q1.name - q2.name)
+          .map((v, idx, arr) => {
+            if (idx > 0) {
+              if (v.ask > arr[0].bid) v.askClass = 'neg';
+              if (v.ask < arr[0].bid) v.askClass = 'pos';
+              if (v.bid > arr[0].ask) v.bidClass = 'pos';
+              if (v.bid < arr[0].ask) v.bidClass = 'neg';
+
+              v.delta = Math.max(arr[0].bid - v.ask, v.bid - arr[0].ask);
+              if (v.delta > 0) v.deltaClass = 'pos';
+              if (v.delta < 0) v.deltaClass = 'neg';
+            }
+
+            return v;
+          });
       }
     })
   },
@@ -38,6 +53,16 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+td {
+  font-family: Monospaced;
 
+  &.neg {
+    color: green;
+  }
+
+  &.pos {
+    color: red;
+  }
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -24,9 +24,3 @@ export default {
   }
 }
 </script>
-
-<style scoped lang="scss">
-table {
-  font-family: Monospaced;
-}
-</style>
