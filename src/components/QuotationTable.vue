d6a04b81317b15d12ed63480fed01dea3ddebeba
Tal <talrasha007@gmail.com>
2019-12-19 22:08:24 +0800

    router-view

---
+++ src/components/QuotationTable.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-simple-table v-slot:default>
+    <thead><tr><th>INS</th><th>Ask</th><th>Bid</th></tr></thead>
+    <tbody>
+
+    </tbody>
+  </v-simple-table>
+</template>
+
+<script>
+export default {
+  name: 'QuotationTable'
+}
+</script>
+
+<style scoped>
+
+</style>
